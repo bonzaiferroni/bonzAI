9006211156488e83ca7de15d6146df0d242e53d0
bonzaiferroni <bonzaiferroni@gmail.com>
2016-11-25 13:20:05 -0700

    road and rampart autolayout

--- src/QuadOperation.ts
+++ src/QuadOperation.ts
@@ -197,20 +197,88 @@ export class QuadOperation extends Operation {
         [STRUCTURE_TERMINAL]: [{x: 0, y: 2}],
         [STRUCTURE_NUKER]: [{x: 0, y: 6}],
         [STRUCTURE_POWER_SPAWN]: [{x: -2, y: 2}],
+        [STRUCTURE_OBSERVER]: [{x: -5, y: 5}],
         [STRUCTURE_LAB]: [
-            {x: -2, y: 4}, {x: -3, y: 3}, {x: -4, y: 2}, {x: -3, y: 4}, {x: -4, y: 4},
-            {x: -5, y: 3}, {x: -2, y: 3}, {x: -3, y: 2}, {x: -4, y: 5}, {x: -5, y: 4}]
+            {x: -2, y: 4}, {x: -3, y: 3}, {x: -4, y: 2}, {x: -3, y: 5}, {x: -4, y: 4},
+            {x: -5, y: 3}, {x: -2, y: 3}, {x: -3, y: 2}, {x: -4, y: 5}, {x: -5, y: 4}],
+        [STRUCTURE_ROAD]: [
+
+            // diamond (n = 12)
+            {x: 3, y: 0}, {x: 2, y: -1}, {x: 1, y: -2}, {x: 0, y: -3}, {x: -1, y: -2},
+            {x: -2, y: -1}, {x: -3, y: 0}, {x: -2, y: 1}, {x: -1, y: 2}, {x: 0, y: 3},
+            {x: 1, y: 2}, {x: 2, y: 1},
+
+            // x-pattern (n = 24)
+            {x: 4, y: 2}, {x: 5, y: -2}, {x: 4, y: -3},
+            {x: 3, y: -4}, {x: 2, y: -5}, {x: 1, y: -4}, {x: -1, y: -4}, {x: -2, y: -5},
+            {x: -3, y: -4}, {x: -4, y: -3}, {x: -5, y: -2}, {x: -4, y: -1}, {x: -4, y: 1},
+            {x: -5, y: -2}, {x: -4, y: 3}, {x: -3, y: 4}, {x: -2, y: 5}, {x: -1, y: 4},
+            {x: 1, y: 4}, {x: 2, y: 5}, {x: 3, y: 4}, {x: 4, y: 3}, {x: 5, y: 2},
+            {x: 4, y: 1},
+
+            // outside (n = 33)
+            {x: 6, y: -3}, {x: 6, y: -4}, {x: 6, y: -5}, {x: 5, y: -6},
+            {x: 4, y: -6}, {x: 3, y: -6}, {x: 1, y: -6}, {x: 0, y: -6}, {x: -1, y: -6},
+            {x: -3, y: -6}, {x: -4, y: -6}, {x: -5, y: -6}, {x: -6, y: -5}, {x: -6, y: -4},
+            {x: -6, y: -3}, {x: -6, y: -1}, {x: -6, y: 0}, {x: -6, y: 1}, {x: -6, y: 3},
+            {x: -6, y: 4}, {x: -6, y: 5}, {x: -5, y: 6}, {x: -4, y: 6}, {x: -3, y: 6},
+            {x: 3, y: 6}, {x: 4, y: 6}, {x: 5, y: 6}, {x: 6, y: 5}, {x: 6, y: 4},
+            {x: 6, y: 3}, {x: 6, y: 1}, {x: 6, y: 0}, {x: 6, y: -1},
+        ],
+        [STRUCTURE_RAMPART]: [
+            // top wall (n = 12)
+            {x: -5, y: -6}, {x: -4, y: -6}, {x: -3, y: -6}, {x: -2, y: -6}, {x: -1, y: -6},
+            {x: 0, y: -6}, {x: 1, y: -6}, {x: 2, y: -6}, {x: 3, y: -6}, {x: 4, y: -6},
+            {x: 5, y: -6}, {x: 5, y: -5},
+
+            // right wall (n = 12)
+            {x: 6, y: -5}, {x: 6, y: -4}, {x: 6, y: -3}, {x: 6, y: -2}, {x: 6, y: -1},
+            {x: 6, y: 0}, {x: 6, y: 1}, {x: 6, y: 2}, {x: 6, y: 3}, {x: 6, y: 4},
+            {x: 6, y: 5}, {x: 5, y: 5},
+
+            // bottom wall (n = 12)
+            {x: 5, y: 6}, {x: 4, y: 6}, {x: 3, y: 6}, {x: 2, y: 6}, {x: 1, y: 6},
+            {x: 0, y: 6}, {x: -1, y: 6}, {x: -2, y: 6}, {x: -3, y: 6}, {x: -4, y: 6},
+            {x: -5, y: 6}, {x: -5, y: 5},
+
+            // left wall (n = 12)
+            {x: -6, y: 5}, {x: -6, y: 4}, {x: -6, y: 3}, {x: -6, y: 2}, {x: -6, y: 1},
+            {x: -6, y: 0}, {x: -6, y: -1}, {x: -6, y: -2}, {x: -6, y: -3}, {x: -6, y: -4},
+            {x: -6, y: -5}, {x: -5, y: -5},
+
+            // storage (n = 1)
+            {x: 0, y: 4}
+        ]
+
+    };
+
+    nonControllerStructures = {
+        [STRUCTURE_RAMPART]: {
+            0: 0, 1: 0, 2: 0, 3: 49, 4: 49, 5: 49, 6: 49, 7: 49, 8: 49,
+        },
+        [STRUCTURE_ROAD]: {
+            0: 0, 1: 12, 2: 12, 3: 12, 4: 36, 5: 36, 6: 36, 7: 69, 8: 69,
+        }
     };
 
     private autoLayout() {
         if (this.memory.centerPoint && this.memory.rotation !== undefined) {
             let centerPosition = new RoomPosition(this.memory.centerPoint.x, this.memory.centerPoint.y, this.flag.room.name);
             for (let structureType in this.layoutDeltas) {
-                let allowedCount = CONTROLLER_STRUCTURES[structureType][this.flag.room.controller.level];
+
+                let allowedCount;
+                if (CONTROLLER_STRUCTURES[structureType]) {
+                    allowedCount = CONTROLLER_STRUCTURES[structureType][this.flag.room.controller.level];
+                }
+                else {
+                    allowedCount = this.nonControllerStructures[structureType]
+                }
+
                 let constructionCount = centerPosition.findInRange(FIND_MY_CONSTRUCTION_SITES, QUAD_RADIUS,
                     {filter: (c: ConstructionSite) => c.structureType === structureType}).length;
                 let count = _.filter(this.flag.room.findStructures(structureType),
                         (s: Structure) => { return centerPosition.inRangeTo(s, QUAD_RADIUS)}).length + constructionCount;
+
                 if (count < allowedCount) {
                     this.findNextConstruction(structureType, allowedCount - count)
                 }
@@ -240,7 +308,7 @@ export class QuadOperation extends Operation {
                 amountOrdered++;
             }
             else {
-                console.log(`bad construction placement: ${outcome}, ${structureType} (${this.name})`, coord.x, coord.y);
+                console.log(`LAYOUT: bad construction placement: ${outcome}, ${structureType}, ${position} (${this.name})`);
             }
 
             if (amountOrdered === amountNeeded) {
@@ -327,11 +395,11 @@ export class QuadOperation extends Operation {
                 }
 
                 if (outcome === OK) {
-                    console.log(`LAYOUT: ${action}d temporary ${action.structureType} (${this.name}, level: ${level})`)
+                    console.log(`LAYOUT: ${action.actionType}d temporary ${action.structureType} (${this.name}, level: ${level})`)
                 }
                 else {
                     console.log(`LAYOUT: problem with temp placement, please follow up in ${this.name}`);
-                    console.log(`tried to ${action} ${action.structureType} at level ${level}, outcome: ${outcome}`);
+                    console.log(`tried to ${action.actionType} ${action.structureType} at level ${level}, outcome: ${outcome}`);
                 }
             }
 
