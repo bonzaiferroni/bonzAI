3ea3d25ddcd4fd942c710c5d7db6029ba0fbf876
bonzaiferroni <bonzaiferroni@gmail.com>
2016-11-28 09:51:11 -0700

    started FlexOperation, to be used as an alternative to QuadOperation for rooms that do not have the contiguous space required by QuadOp

---
+++ src/FlexOperation.ts
@@ -0,0 +1,17 @@
+import {ControllerOperation} from "./ControllerOperation";
+export class FlexOperation extends ControllerOperation {
+
+    protected addDefense() {
+    }
+
+    protected temporaryPlacement(controllerLevel: number) {
+    }
+
+    protected repairWalls() {
+    }
+
+    protected findStructureCount(structureType: string): number {
+        return undefined;
+    }
+
+}
