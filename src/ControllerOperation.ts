04a88039b6e6b2213543c0900dee1a4c8da14dce
bonzaiferroni <bonzaiferroni@gmail.com>
2016-11-28 09:10:55 -0700

    created abstract class for auto-layout rooms

---
+++ src/ControllerOperation.ts
@@ -0,0 +1,240 @@
+import {Operation} from "./Operation";
+import {EmergencyMinerMission} from "./EmergencyMission";
+import {RefillMission} from "./RefillMission";
+import {NightsWatchMission} from "./NightsWatchMission";
+import {PowerMission} from "./PowerMission";
+import {TerminalNetworkMission} from "./TerminalNetworkMission";
+import {IgorMission} from "./IgorMission";
+import {LinkMiningMission} from "./LinkMiningMission";
+import {MiningMission} from "./MiningMission";
+import {BuildMission} from "./BuildMission";
+import {LinkNetworkMission} from "./LinkNetworkMission";
+import {GeologyMission} from "./GeologyMission";
+import {UpgradeMission} from "./UpgradeMission";
+import {PaverMission} from "./PaverMission";
+import {Coord} from "./interfaces";
+import {NEED_ENERGY_THRESHOLD, ENERGYSINK_THRESHOLD} from "./constants";
+export abstract class ControllerOperation extends Operation {
+
+    protected abstract addDefense();
+    protected abstract temporaryPlacement(controllerLevel: number);
+    protected abstract repairWalls();
+    protected abstract findStructureCount(structureType: string): number;
+
+    allowedCount: {
+        [structureType: string]: {
+            [controllerLevel: number]: number
+        }
+    };
+    layoutMap: {
+        [structureType: string]: Coord[]
+    };
+
+    initOperation() {
+
+        if (!this.flag.room) return; // TODO: remote revival
+
+        // initOperation FortOperation variables
+        this.spawnGroup = this.empire.getSpawnGroup(this.flag.room.name);
+        this.empire.register(this.flag.room);
+
+        // spawn emergency miner if needed
+        this.addMission(new EmergencyMinerMission(this));
+
+        // refill spawning energy - will spawn small spawnCart if needed
+        let structures = this.flag.room.findStructures(STRUCTURE_EXTENSION)
+            .concat(this.flag.room.find(FIND_MY_SPAWNS)) as Structure[];
+        let maxCarts = this.flag.room.storage ? 1 : 2;
+        this.addMission(new RefillMission(this, "spawnCart", maxCarts, structures, 10, true));
+
+        this.addDefense();
+
+        if (this.memory.powerMining) {
+            this.addMission(new PowerMission(this));
+        }
+
+        // energy network
+        if (this.flag.room.terminal && this.flag.room.storage) {
+            this.addMission(new TerminalNetworkMission(this));
+            this.addMission(new IgorMission(this));
+        }
+
+        // harvest energy
+        for (let i = 0; i < this.sources.length; i++) {
+            if (this.sources[i].pos.lookFor(LOOK_FLAGS).length > 0) continue;
+            let source = this.sources[i];
+            if (this.flag.room.controller.level === 8 && this.flag.room.storage) {
+                let link = source.findMemoStructure(STRUCTURE_LINK, 2) as StructureLink;
+                if (link) {
+                    this.addMission(new LinkMiningMission(this, "miner" + i, source, link));
+                    continue;
+                }
+            }
+            this.addMission(new MiningMission(this, "miner" + i, source));
+        }
+
+        // build construction
+        let allowBuilderSpawn = this.flag.room.find(FIND_MY_CONSTRUCTION_SITES).length > 0;
+        this.addMission(new BuildMission(this, "builder", this.calcBuilderPotency(), allowBuilderSpawn));
+
+        // use link array near storage to fire energy at controller link (pre-rcl8)
+        if (this.flag.room.storage) {
+            this.addMission(new LinkNetworkMission(this));
+
+            let extractor = this.mineral.pos.lookFor<StructureExtractor>(LOOK_STRUCTURES)[0];
+            if (this.flag.room.energyCapacityAvailable > 5000 && extractor && extractor.my) {
+                this.addMission(new GeologyMission(this));
+            }
+        }
+
+        // upgrader controller
+        let boostUpgraders = this.flag.room.controller.level < 8;
+        this.addMission(new UpgradeMission(this, boostUpgraders));
+
+        // repair roads
+        this.addMission(new PaverMission(this));
+
+        this.autoLayout();
+        this.repairWalls();
+    }
+
+    finalizeOperation() {
+    }
+
+    invalidateOperationCache() {
+        this.memory.masonPotency = undefined;
+        this.memory.builderPotency = undefined;
+    }
+
+    calcMasonPotency(): number {
+        if (!this.memory.masonPotency) {
+            let surplusMode = this.flag.room.storage && this.flag.room.storage.store.energy > NEED_ENERGY_THRESHOLD;
+            let megaSurplusMode = this.flag.room.storage && this.flag.room.storage.store.energy > ENERGYSINK_THRESHOLD;
+            let potencyBasedOnStorage = megaSurplusMode ? 10 : surplusMode ? 5 : 1;
+
+            if (this.memory.wallBoost) {
+                potencyBasedOnStorage = 20;
+            }
+
+            // would happen to be the same as the potency used for builders
+            let potencyBasedOnSpawn = this.calcBuilderPotency();
+
+            if (this.memory.wallBoost) {
+                this.memory.mason.activateBoost = true;
+            }
+
+            this.memory.masonPotency = Math.min(potencyBasedOnSpawn, potencyBasedOnStorage);
+        }
+        return this.memory.masonPotency;
+    }
+
+    calcBuilderPotency(): number {
+        if (!this.memory.builderPotency) {
+            this.memory.builderPotency = Math.min(Math.floor(this.spawnGroup.maxSpawnEnergy / 175), 20);
+        }
+        return this.memory.builderPotency;
+    }
+
+    public nuke(x: number, y: number, roomName: string): string {
+        let nuker = _.head(this.flag.room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_NUKER}})) as StructureNuker;
+        let outcome = nuker.launchNuke(new RoomPosition(x, y, roomName));
+        if (outcome === OK) {
+            this.empire.addNuke({tick: Game.time, roomName: roomName});
+            return "NUKER: Bombs away! \\o/";
+        }
+        else {
+            return `NUKER: error: ${outcome}`;
+        }
+    }
+
+    addAllyRoom(roomName: string) {
+        if (_.includes(this.memory.network.scanData.roomNames, roomName)) {
+            return "NETWORK: " + roomName + " is already being scanned by " + this.name;
+        }
+
+        this.memory.network.scanData.roomNames.push(roomName);
+        this.empire.addAllyForts([roomName]);
+        return "NETWORK: added " + roomName + " to rooms scanned by " + this.name;
+    }
+
+    private autoLayout() {
+        if (this.memory.centerPoint && this.memory.rotation !== undefined) {
+            let centerPosition = new RoomPosition(this.memory.centerPoint.x, this.memory.centerPoint.y, this.flag.room.name);
+
+            if (this.memory.checkLayoutIndex === undefined || this.memory.checkLayoutIndex >= Object.keys(this.layoutMap).length) {
+                this.memory.checkLayoutIndex = 0;
+            }
+
+            let structureType = Object.keys(this.layoutMap)[this.memory.checkLayoutIndex++];
+
+            let allowedCount;
+            if (CONTROLLER_STRUCTURES[structureType][this.flag.room.controller.level] < 100) {
+                allowedCount = CONTROLLER_STRUCTURES[structureType][this.flag.room.controller.level];
+            }
+            else {
+                allowedCount = this.allowedCount[structureType][this.flag.room.controller.level];
+            }
+
+            let count = this.findStructureCount(structureType);
+
+            if (count < allowedCount) {
+                console.log(structureType, allowedCount, count);
+                this.findNextConstruction(structureType, allowedCount - count)
+            }
+
+            this.temporaryPlacement(this.flag.room.controller.level);
+        }
+    }
+
+    private findNextConstruction(structureType: string, amountNeeded: number) {
+        let amountOrdered = 0;
+
+        for (let coord of this.layoutMap[structureType]) {
+            let position = this.coordToPosition(coord);
+            if (!position) {
+                console.log(`LAYOUT: bad position, is centerPoint misplaced? (${this.name})`);
+                return;
+            }
+            let hasStructure = position.lookForStructure(structureType);
+            if (hasStructure) continue;
+            let hasConstruction = position.lookFor(LOOK_CONSTRUCTION_SITES)[0];
+            if (hasConstruction) continue;
+
+            let outcome = position.createConstructionSite(structureType);
+            if (outcome === OK) {
+                console.log(`LAYOUT: placing ${structureType} at ${position} (${this.name})`);
+                amountOrdered++;
+            }
+            else {
+                console.log(`LAYOUT: bad construction placement: ${outcome}, ${structureType}, ${position} (${this.name})`);
+            }
+
+            if (amountOrdered === amountNeeded) {
+                console.log(`LAYOUT: finished placing construction for: ${structureType} (${this.name})`);
+                break;
+            }
+        }
+    }
+
+    protected coordToPosition(coord: Coord) {
+        let centerPoint = this.memory.centerPoint;
+        let rotation = this.memory.rotation;
+
+        let xCoord = coord.x;
+        let yCoord = coord.y;
+        if (rotation === 1) {
+            xCoord = -coord.y;
+            yCoord = coord.x;
+        }
+        else if (rotation === 2) {
+            xCoord = -coord.y;
+            yCoord = -coord.x;
+        }
+        else if (rotation === 3) {
+            xCoord = coord.y;
+            yCoord = -coord.x;
+        }
+        return new RoomPosition(centerPoint.x + xCoord, centerPoint.y + yCoord, this.flag.room.name);
+    }
+
+}
--- src/QuadOperation.ts
+++ src/QuadOperation.ts
@@ -1,26 +1,13 @@
-import {Operation} from "./Operation";
 import {Empire} from "./Empire";
-import {BuildMission} from "./BuildMission";
-import {EmergencyMinerMission} from "./EmergencyMission";
-import {GeologyMission} from "./GeologyMission";
-import {IgorMission} from "./IgorMission";
-import {LinkMiningMission} from "./LinkMiningMission";
-import {LinkNetworkMission} from "./LinkNetworkMission";
 import {NightsWatchMission} from "./NightsWatchMission";
-import {MiningMission} from "./MiningMission";
-import {PaverMission} from "./PaverMission";
-import {PowerMission} from "./PowerMission";
-import {RefillMission} from "./RefillMission";
-import {TerminalNetworkMission} from "./TerminalNetworkMission";
-import {UpgradeMission} from "./UpgradeMission";
-
 import {OperationPriority, NEED_ENERGY_THRESHOLD, ENERGYSINK_THRESHOLD} from "./constants";
 import {Coord} from "./interfaces";
+import {ControllerOperation} from "./ControllerOperation";
 
 const QUAD_RADIUS = 6;
 const REPAIR_INTERVAL = 4;
 
-export class QuadOperation extends Operation {
+export class QuadOperation extends ControllerOperation {
 
     memory: {
         powerMining: boolean
@@ -50,133 +37,16 @@ export class QuadOperation extends Operation {
         this.priority = OperationPriority.OwnedRoom;
     }
 
-    initOperation() {
-        if (this.flag.room) {
-
-            // initOperation FortOperation variables
-            this.spawnGroup = this.empire.getSpawnGroup(this.flag.room.name);
-            this.empire.register(this.flag.room);
-
-            // spawn emergency miner if needed
-            this.addMission(new EmergencyMinerMission(this));
-
-            // refill spawning energy - will spawn small spawnCart if needed
-            let structures = this.flag.room.findStructures(STRUCTURE_EXTENSION)
-                .concat(this.flag.room.find(FIND_MY_SPAWNS)) as Structure[];
-            let maxCarts = this.flag.room.storage ? 1 : 2;
-            this.addMission(new RefillMission(this, "spawnCart", maxCarts, structures, 10, true));
-
-            this.addMission(new NightsWatchMission(this));
-
-            if (this.memory.powerMining) {
-                this.addMission(new PowerMission(this));
-            }
-
-            // energy network
-            if (this.flag.room.terminal && this.flag.room.storage) {
-                this.addMission(new TerminalNetworkMission(this));
-                this.addMission(new IgorMission(this));
-            }
-
-            // harvest energy
-            for (let i = 0; i < this.sources.length; i++) {
-                if (this.sources[i].pos.lookFor(LOOK_FLAGS).length > 0) continue;
-                let source = this.sources[i];
-                if (this.flag.room.controller.level === 8 && this.flag.room.storage) {
-                    let link = source.findMemoStructure(STRUCTURE_LINK, 2) as StructureLink;
-                    if (link) {
-                        this.addMission(new LinkMiningMission(this, "miner" + i, source, link));
-                        continue;
-                    }
-                }
-                this.addMission(new MiningMission(this, "miner" + i, source));
-            }
-
-            // build construction
-            let allowBuilderSpawn = this.flag.room.find(FIND_MY_CONSTRUCTION_SITES).length > 0;
-            this.addMission(new BuildMission(this, "builder", this.calcBuilderPotency(), allowBuilderSpawn));
-
-            // use link array near storage to fire energy at controller link (pre-rcl8)
-            if (this.flag.room.storage) {
-                this.addMission(new LinkNetworkMission(this));
-
-                let extractor = this.mineral.pos.lookFor<StructureExtractor>(LOOK_STRUCTURES)[0];
-                if (this.flag.room.energyCapacityAvailable > 5000 && extractor && extractor.my) {
-                    this.addMission(new GeologyMission(this));
-                }
-            }
-
-            // upgrader controller
-            let boostUpgraders = this.flag.room.controller.level < 8;
-            this.addMission(new UpgradeMission(this, boostUpgraders));
-
-            // repair roads
-            this.addMission(new PaverMission(this));
-
-            this.autoLayout();
-            this.repairWall();
-        }
+    protected addDefense() {
+        this.addMission(new NightsWatchMission(this));
     }
 
     finalizeOperation() {
     }
     invalidateOperationCache() {
-        this.memory.masonPotency = undefined;
-        this.memory.builderPotency = undefined;
     }
 
-    calcMasonPotency(): number {
-        if (!this.memory.masonPotency) {
-            let surplusMode = this.flag.room.storage && this.flag.room.storage.store.energy > NEED_ENERGY_THRESHOLD;
-            let megaSurplusMode = this.flag.room.storage && this.flag.room.storage.store.energy > ENERGYSINK_THRESHOLD;
-            let potencyBasedOnStorage = megaSurplusMode ? 10 : surplusMode ? 5 : 1;
-
-            if (this.memory.wallBoost) {
-                potencyBasedOnStorage = 20;
-            }
-
-            // would happen to be the same as the potency used for builders
-            let potencyBasedOnSpawn = this.calcBuilderPotency();
-
-            if (this.memory.wallBoost) {
-                this.memory.mason.activateBoost = true;
-            }
-
-            this.memory.masonPotency = Math.min(potencyBasedOnSpawn, potencyBasedOnStorage);
-        }
-        return this.memory.masonPotency;
-    }
-
-    calcBuilderPotency(): number {
-        if (!this.memory.builderPotency) {
-            this.memory.builderPotency = Math.min(Math.floor(this.spawnGroup.maxSpawnEnergy / 175), 20);
-        }
-        return this.memory.builderPotency;
-    }
-
-    public nuke(x: number, y: number, roomName: string): string {
-        let nuker = _.head(this.flag.room.find(FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_NUKER}})) as StructureNuker;
-        let outcome = nuker.launchNuke(new RoomPosition(x, y, roomName));
-        if (outcome === OK) {
-            this.empire.addNuke({tick: Game.time, roomName: roomName});
-            return "NUKER: Bombs away! \\o/";
-        }
-        else {
-            return `NUKER: error: ${outcome}`;
-        }
-    }
-
-    addAllyRoom(roomName: string) {
-        if (_.includes(this.memory.network.scanData.roomNames, roomName)) {
-            return "NETWORK: " + roomName + " is already being scanned by " + this.name;
-        }
-
-        this.memory.network.scanData.roomNames.push(roomName);
-        this.empire.addAllyForts([roomName]);
-        return "NETWORK: added " + roomName + " to rooms scanned by " + this.name;
-    }
-
-    layoutDeltas = {
+    layoutMap = {
         [STRUCTURE_SPAWN]: [{x: 2, y: 0}, {x: 0, y: -2}, {x: -2, y: 0}],
         [STRUCTURE_TOWER]: [
             {x: 1, y: -1}, {x: -1, y: -1}, {x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}],
@@ -252,7 +122,7 @@ export class QuadOperation extends Operation {
 
     };
 
-    nonControllerStructures = {
+    allowedCount = {
         [STRUCTURE_RAMPART]: {
             0: 0, 1: 0, 2: 0, 3: 0, 4: 49, 5: 49, 6: 49, 7: 49, 8: 49,
         },
@@ -261,90 +131,7 @@ export class QuadOperation extends Operation {
         }
     };
 
-    private autoLayout() {
-        if (this.memory.centerPoint && this.memory.rotation !== undefined) {
-            let centerPosition = new RoomPosition(this.memory.centerPoint.x, this.memory.centerPoint.y, this.flag.room.name);
-
-            if (this.memory.checkLayoutIndex === undefined || this.memory.checkLayoutIndex >= Object.keys(this.layoutDeltas).length) {
-                this.memory.checkLayoutIndex = 0;
-            }
-
-            let structureType = Object.keys(this.layoutDeltas)[this.memory.checkLayoutIndex++];
-
-            let allowedCount;
-            if (CONTROLLER_STRUCTURES[structureType][this.flag.room.controller.level] < 100) {
-                allowedCount = CONTROLLER_STRUCTURES[structureType][this.flag.room.controller.level];
-            }
-            else {
-                allowedCount = this.nonControllerStructures[structureType][this.flag.room.controller.level];
-            }
-
-            let constructionCount = centerPosition.findInRange(FIND_MY_CONSTRUCTION_SITES, QUAD_RADIUS,
-                {filter: (c: ConstructionSite) => c.structureType === structureType}).length;
-            let count = _.filter(this.flag.room.findStructures(structureType),
-                    (s: Structure) => { return centerPosition.inRangeTo(s, QUAD_RADIUS)}).length + constructionCount;
-
-            if (count < allowedCount) {
-                console.log(structureType, allowedCount, count);
-                this.findNextConstruction(structureType, allowedCount - count)
-            }
-
-            this.temporaryPlacement(this.flag.room.controller.level);
-        }
-    }
-
-    private findNextConstruction(structureType: string, amountNeeded: number) {
-        let amountOrdered = 0;
-
-        for (let coord of this.layoutDeltas[structureType]) {
-            let position = this.coordToPosition(coord);
-            if (!position) {
-                console.log(`LAYOUT: bad position, is centerPoint misplaced? (${this.name})`);
-                return;
-            }
-            let hasStructure = position.lookForStructure(structureType);
-            if (hasStructure) continue;
-            let hasConstruction = position.lookFor(LOOK_CONSTRUCTION_SITES)[0];
-            if (hasConstruction) continue;
-
-            let outcome = position.createConstructionSite(structureType);
-            if (outcome === OK) {
-                console.log(`LAYOUT: placing ${structureType} at ${position} (${this.name})`);
-                amountOrdered++;
-            }
-            else {
-                console.log(`LAYOUT: bad construction placement: ${outcome}, ${structureType}, ${position} (${this.name})`);
-            }
-
-            if (amountOrdered === amountNeeded) {
-                console.log(`LAYOUT: finished placing construction for: ${structureType} (${this.name})`);
-                break;
-            }
-        }
-    }
-
-    private coordToPosition(coord: Coord) {
-        let centerPoint = this.memory.centerPoint;
-        let rotation = this.memory.rotation;
-
-        let xCoord = coord.x;
-        let yCoord = coord.y;
-        if (rotation === 1) {
-            xCoord = -coord.y;
-            yCoord = coord.x;
-        }
-        else if (rotation === 2) {
-            xCoord = -coord.y;
-            yCoord = -coord.x;
-        }
-        else if (rotation === 3) {
-            xCoord = coord.y;
-            yCoord = -coord.x;
-        }
-        return new RoomPosition(centerPoint.x + xCoord, centerPoint.y + yCoord, this.flag.room.name);
-    }
-
-    private repairWall() {
+    protected repairWalls() {
         if (Game.time % REPAIR_INTERVAL !== 0) return;
 
         let towers = this.flag.room.findStructures(STRUCTURE_TOWER) as StructureTower[];
@@ -356,7 +143,7 @@ export class QuadOperation extends Operation {
         rampart.pos.findClosestByRange<StructureTower>(towers).repair(rampart);
     }
 
-    private temporaryPlacement(level: number) {
+    protected temporaryPlacement(level: number) {
         if (!this.memory.temporaryPlacement) this.memory.temporaryPlacement = {};
         if (!this.memory.temporaryPlacement[level]) {
 
@@ -412,5 +199,16 @@ export class QuadOperation extends Operation {
 
             this.memory.temporaryPlacement[level] = true;
         }
+    }
+
+    protected findStructureCount(structureType: string): number {
+        let centerPosition = new RoomPosition(this.memory.centerPoint.x, this.memory.centerPoint.y, this.flag.room.name);
+
+        let constructionCount = centerPosition.findInRange(FIND_MY_CONSTRUCTION_SITES, QUAD_RADIUS,
+            {filter: (c: ConstructionSite) => c.structureType === structureType}).length;
+        let count = _.filter(this.flag.room.findStructures(structureType),
+                (s: Structure) => { return centerPosition.inRangeTo(s, QUAD_RADIUS)}).length + constructionCount;
+
+        return count;
     }
 }
