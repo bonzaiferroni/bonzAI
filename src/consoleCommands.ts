77b7cc8ea77e7094fe4032b9864f1bcb7da3939e
bonzaiferroni <bonzaiferroni@gmail.com>
2016-12-01 10:03:43 -0700

    removing construction sites allows you to keep those that already have progress

--- src/consoleCommands.ts
+++ src/consoleCommands.ts
@@ -15,9 +15,9 @@ export var consoleCommands = {
         })
     },
 
-    removeConstructionSites(roomName: string, structureType?: string) {
+    removeConstructionSites(roomName: string, leaveProgressStarted = true, structureType?: string) {
         Game.rooms[roomName].find(FIND_MY_CONSTRUCTION_SITES).forEach( (site: ConstructionSite) => {
-            if (!structureType || site.structureType === structureType) {
+            if ((!structureType || site.structureType === structureType) &&(!leaveProgressStarted || site.progress === 0)) {
                 site.remove();
             }
         })
