67b0019754b0f8004ab12f9801b9e1bf21700fe1
bonzaiferroni <bonzaiferroni@gmail.com>
2016-12-06 09:14:04 -0700

    added temporary layout to FlexOperation

--- src/FlexOperation.ts
+++ src/FlexOperation.ts
@@ -1,17 +1,11 @@
 import {ControllerOperation} from "./ControllerOperation";
 import {Coord} from "./interfaces";
 import {DefenseMission} from "./DefenseMission";
-import {OperationPriority} from "./constants";
-import {Empire} from "./Empire";
 import {BuildMission} from "./BuildMission";
 import {FlexGenerator} from "./FlexGenerator";
+import {helper} from "./helper";
 
 export class FlexOperation extends ControllerOperation {
-
-    constructor(flag: Flag, name: string, type: string, empire: Empire) {
-        super(flag, name, type, empire);
-        this.priority = OperationPriority.OwnedRoom;
-    }
 
     protected repairStructures() {
         if (this.flag.room.findStructures(STRUCTURE_RAMPART).length > 0 && !this.memory.noMason) {
@@ -23,7 +17,54 @@ export class FlexOperation extends ControllerOperation {
         this.addMission(new DefenseMission(this));
     }
 
-    protected temporaryPlacement(controllerLevel: number) {
+    protected temporaryPlacement(level: number) {
+        if (!this.memory.temporaryPlacement) this.memory.temporaryPlacement = {};
+        if (!this.memory.temporaryPlacement[level]) {
+
+            let actions: {actionType: string, structureType: string, coord: Coord}[] = [];
+
+            // links
+            if (level === 5) {
+                actions.push({actionType: "place", structureType: STRUCTURE_LINK, coord: {x: 2, y: -1}});
+            }
+            if (level === 6) {
+                actions.push({actionType: "place", structureType: STRUCTURE_LINK, coord: {x: 1, y: -1}});
+            }
+            if (level === 7) {
+                actions.push({actionType: "place", structureType: STRUCTURE_LINK, coord: {x: 0, y: -1}});
+            }
+            if (level === 8) {
+                actions.push({actionType: "remove", structureType: STRUCTURE_LINK, coord: {x: 1, y: -1}});
+                actions.push({actionType: "remove", structureType: STRUCTURE_LINK, coord: {x: 0, y: -1}});
+            }
+
+            for (let action of actions) {
+                let outcome;
+                let position = helper.coordToPosition(action.coord, this.memory.centerPosition, this.memory.rotation);
+                if (action.actionType === "place") {
+                    outcome = position.createConstructionSite(action.structureType);
+                }
+                else {
+                    let structure = position.lookForStructure(action.structureType);
+                    if (structure) {
+                        outcome = structure.destroy();
+                    }
+                    else {
+                        outcome = "noStructure";
+                    }
+                }
+
+                if (outcome === OK) {
+                    console.log(`LAYOUT: ${action.actionType}d temporary ${action.structureType} (${this.name}, level: ${level})`)
+                }
+                else {
+                    console.log(`LAYOUT: problem with temp placement, please follow up in ${this.name}`);
+                    console.log(`tried to ${action.actionType} ${action.structureType} at level ${level}, outcome: ${outcome}`);
+                }
+            }
+
+            this.memory.temporaryPlacement[level] = true;
+        }
     }
 
     protected initAutoLayout() {
