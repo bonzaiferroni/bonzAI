d55a21d4d0d7075e58923b66381b8b966f956113
bonzaiferroni <bonzaiferroni@gmail.com>
2016-12-31 09:57:51 -0700

    find a guaranteed open position near the middle of a room

--- src/helpers/helper.ts
+++ src/helpers/helper.ts
@@ -356,5 +356,23 @@ export var helper = {
             lastPosition = position;
         }
         return serializedPath;
+    },
+
+    pathablePosition(roomName: string): RoomPosition {
+        for (let radius = 0; radius < 20; radius++) {
+            for (let xDelta = -radius; xDelta <= radius; xDelta++) {
+                for (let yDelta = -radius; yDelta <= radius; yDelta++) {
+                    if (Math.abs(yDelta) !== radius && Math.abs(xDelta) !== radius) {
+                        continue;
+                    }
+                    let x = 25 + xDelta;
+                    let y = 25 + yDelta;
+                    let terrain = Game.map.getTerrainAt(x, y, roomName);
+                    if (terrain !== "wall") {
+                        return new RoomPosition(x, y, roomName);
+                    }
+                }
+            }
+        }
     }
 };
